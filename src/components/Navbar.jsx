7c77104e4b9ecec512bcb8c1e32a8d30a88eff83
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-06-20 14:27:30 +0300

    Added a logo in the navbar and a favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import { Link } from 'react-router-dom';
+import logo from '../images/main-logo.jpg';
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
       <div className="container-fluid">
+        <a className="navbar-brand d-flex align-items-center" href="/">
+          <img
+          src={logo}
+          alt='logo'
+          width={40}
+          height={40}
+          className="d-inline-block align-text-top me-2" />
+        </a>
         <Link className="navbar-brand fs-3" to="/">Greekend Transfers</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
